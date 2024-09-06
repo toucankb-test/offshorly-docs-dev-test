284ab9a56b668dd250d9c7ae9bf54fc3cb98a9d1
Clarence Coronel <clarence.coronel.r@gmail.com>
2024-09-06 14:15:39 +0800

    refactor: change approach to deletion

--- scripts/checkDeletedFile.ts
+++ scripts/checkDeletedFile.ts
@@ -1,47 +1,6 @@
-import { execSync } from 'child_process'
+import getMdPaths from "./getMdPaths.js";
 
-// async function getDeletedFiles() {
-//   try {
-//     // compare the current HEAD with the last commit on the main branch before the push
-//     const diffOutput = execSync(
-//       'git diff --name-status origin/main...HEAD',
-//     ).toString()
-//     const deletedFiles = diffOutput
-//       .split('\n')
-//       .filter((line) => line.startsWith('D'))
-//       .map((line) => line.split('\t')[1])
-//       .filter((filePath) => filePath.length > 0)
-//     return { deleted: deletedFiles }
-//   } catch (error) {
-//     console.error('Error getting deleted files:', error)
-//     return { deleted: [] }
-//   }
-// }
-
-interface DeletedFilesResult {
-  deleted: string[];
-}
-
-function getDeletedFiles(): DeletedFilesResult {
-  try {
-    // Fetch the latest changes from the main branch
-    execSync('git fetch origin main', { stdio: 'inherit' });
-
-    // Get the list of deleted files
-    const result = execSync('git diff --name-status origin/main', { encoding: 'utf8' });
-    const deletedFiles = result
-      .split('\n')
-      .filter(line => line.startsWith('D'))
-      .map(line => line.split('\t')[1]);
-
-    return { deleted: deletedFiles };
-  } catch (error) {
-    console.error('Error detecting deleted files:', error);
-    process.exit(1);
-  }
-}
-
-async function sendDeletedFiles(deletedFiles: any) {
+async function syncDeletedFiles(paths: string[]) {
   const endpoint = 'https://kb-backend-dev.onrender.com/api/delete-markdown'
   const apiKey = process.env.CODEBASE_KB_API_KEY;
   try {
@@ -51,7 +10,7 @@ async function sendDeletedFiles(deletedFiles: any) {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${apiKey}`
        },
-      body: JSON.stringify({ deleted: deletedFiles }),
+      body: JSON.stringify({ paths }),
     })
     const data = await response.json()
     console.log('Response from server:', data)
@@ -61,15 +20,15 @@ async function sendDeletedFiles(deletedFiles: any) {
 }
 
 async function run() {
-  const deletedFiles = await getDeletedFiles()
-  if (deletedFiles.deleted.length > 0) {
-    await sendDeletedFiles(deletedFiles)
+  // Collect all md paths
+  const paths = getMdPaths('content', [])
+
+  if (paths.length > 0) {
+    // Check for markdown metadata in db that does not exist
+    syncDeletedFiles(paths)
   } else {
-    console.log('No deleted files found.')
+    console.log('No markdown files found.')
   }
 }
 
 run(); // Start the process
-
-// For debugging purposes, you can call getDeletedFiles separately and log the result
-// getDeletedFiles().then((deletesFile) => console.log(deletesFile))
